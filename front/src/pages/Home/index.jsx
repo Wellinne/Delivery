6aa24783f69d724f4df0f62a2025a3bebc7bed1c
Wellinne Nascimento <wellinnenascimento@gmail.com>
2023-02-13 20:11:27 -0300

    tela de construção para primeira apresentação do projeto

---
+++ front/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Logo } from "../../assets";
+import './style.css';
+
+export const Home = () => {
+    return (
+        <div>
+            <img src={Logo} alt="Logo" width={600}/>
+            <h1>Em construção...</h1>
+        </div>
+    )
+}
